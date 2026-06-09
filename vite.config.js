00a9495ff299f1ce4ae26c76dc5ce126dcb5d1ae
Rodgen <genyelicaros@gmail.com>
2026-06-10 02:51:57 +0800

    fix: remove 'express-document-uploader' from build inputs in Vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
         'privacy-policy': resolve(__dirname, 'privacy-policy.html'),
         'terms-of-service': resolve(__dirname, 'terms-of-service.html'),
         'coming-soon': resolve(__dirname, 'coming-soon.html'),
-        'express-document-uploader': resolve(__dirname, 'express-document-uploader.html'),
       }
     }
   }
