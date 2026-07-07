f2fd3484b821957c6f0cd5739c02d4963be87c54
Rodgen <genyelicaros@gmail.com>
2026-07-07 12:27:59 -0600

    early rrpayment page building

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         blog: resolve(__dirname, 'blog.html'),
+        'early-repayment': resolve(__dirname, 'early-repayment.html'),
         faq: resolve(__dirname, 'faq.html'),
         'privacy-policy': resolve(__dirname, 'privacy-policy.html'),
         'terms-of-service': resolve(__dirname, 'terms-of-service.html'),
